3137cb63ccf615177e29bf7132f958e73a6930d9
drishtiisharma <drishti.1106@gmail.com>
2026-01-10 21:29:26 +0530

    new scope

--- backend/services/auth.py
+++ backend/services/auth.py
@@ -17,6 +17,7 @@ SCOPES = [
     "https://www.googleapis.com/auth/userinfo.email",
     "https://www.googleapis.com/auth/userinfo.profile",
     "openid",
+    "https://www.googleapis.com/auth/gmail.readonly",
     "https://www.googleapis.com/auth/gmail.modify",
     "https://www.googleapis.com/auth/calendar"
 ]
